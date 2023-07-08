df30210d2d13a878f830b61faa66ca99f9847f2d
Vinicius Coelho <50638499+vini-coelho@users.noreply.github.com>
2023-07-08 11:01:19 -0400

    Initial Commit

---
+++ ShoppingList Watch App/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList Watch App
+//
+//  Created by Vinicius Coelho on 08/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingList_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
